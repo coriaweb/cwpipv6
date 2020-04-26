9f7b0a6d93a442183c6ce03967ff0add222acd70
coriaweb <info@coriaweb.hosting>
2020-04-26 16:02:59 +0200

    Create 3rdparty.php

---
+++ usr/local/cwpsrv/htdocs/resources/admin/include/3rdparty.php
@@ -0,0 +1,14 @@
+
+        
+        <!-- cwp_ipv6 -->
+
+</ul>
+<li class="custom-menu"> <!-- this class "custom-menu" was added so you can remove the Developer Menu easily if you want -->
+    <a class="hasUl" href="#"><span class="icon16 icomoon-icon-hammer"></span>IPv6<span class="hasDrop icon16 icomoon-icon-arrow-down-2"></span></a>
+    <ul class="sub">
+<li><a href="index.php?module=ipv6"><span class="icon16 icomoon-icon-arrow-right-3"></span>Asignar direcciones IPv6</a></li>
+<li><a href="index.php?module=ipv6_list"><span class="icon16 icomoon-icon-arrow-right-3"></span>Rangos IPv6</a></li>
+</ul>
+</li>
+<li style="display:none;"><ul>
+<!-- end ipv6 -->
