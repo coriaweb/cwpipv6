8783a08708fea58f4793b208c133d9deb816e218
rcschaff82 <30818412+rcschaff82@users.noreply.github.com>
2020-05-15 07:35:32 -0400

    Fixed Install Script

--- install.sh
+++ install.sh
@@ -10,7 +10,11 @@ cat <<'EOF' >> /usr/local/cwpsrv/htdocs/resources/admin/include/3rdparty.php
 <noscript>
 </ul>
 <li class="custom-menu"> <!-- this class "custom-menu" was added so you can remove the Developer Menu easily if you want -->
-    <a href="?module=icecast"><span class="icon16 icomoon-icon-volume-high"></span>Icecast</a>
+    <a href="#"><span class="icon16 icomoon-icon-hammer"></span>IPV6 Module</a>
+	< ul  class = " sub " >
+< Li > < a  href = " index.php? Module = ipv6 " > < span  class = " icon16 icomoon-icon-arrow-right-3 " > </ span > Assign IPv6 addresses </ a > </ li >
+< Li > < a  href = " index.php? Module = ipv6_list " > < span  class = " icon16 icomoon-icon-arrow-right-3 " > </ span > Ranges IPv6 </ a > </ li >
+</ ul >
 </li>
 <li style="display:none;"><ul>
 </noscript>
@@ -18,8 +22,11 @@ cat <<'EOF' >> /usr/local/cwpsrv/htdocs/resources/admin/include/3rdparty.php
         $(document).ready(function() {
                 var newButtons = ''
                 +' <li>'
-                +' <a href="?module=icecast" class=""><span aria-hidden="true" class="icon16 icomoon-icon-volume-high"></span>Icecast</a>'
-                +'</li>';
+                +'<a href="?module=ipv6" class=""><span aria-hidden="true" class="icon16 icomoon-icon-hammer"></span>IPV6 Module</a>'
+		+'<ul class = "sub">'
+		+'<li> <a href = "index.php?module=ipv6"> <span class="icon16 icomoon-icon-arrow-right-3"></span>Assign IPv6 addresses</a></li>'
+                +'<li> <a href = "index.php?module=ipv6_list"> <span class="icon16 icomoon-icon-arrow-right-3"></span>Ranges IPv6</a></li>'
+		+'</ul></li>';
                 $("li#mn-3").before(newButtons);
         });
 </script>
